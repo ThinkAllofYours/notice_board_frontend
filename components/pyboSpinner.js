63dcea95c4739fef15f68ec12daa187de1dc3a62
DuhwanJang <lyder86@puroom.net>
2023-04-05 12:44:38 +0900

    edit loading bar and remove next export

--- components/pyboSpinner.js
+++ components/pyboSpinner.js
@@ -3,7 +3,6 @@ import Spinner from 'react-bootstrap/Spinner';
 function PyboLodingBar() {
   return (
     <Spinner animation="border" role="status">
-      <span>Loading...</span>
     </Spinner>
   );
 }
